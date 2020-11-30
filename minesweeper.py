923c731fc253f40a14a4c2c4b7c33ad65b7aa7e9
FaridLazuarda <faridadilazuarda29@gmail.com>
2020-11-30 21:48:06 +0700

    update text input

--- minesweeper.py
+++ minesweeper.py
@@ -2,13 +2,13 @@ from clipspy import ClipsMinesweeper
 from gui import *
 
 class Minesweeper():
-    def __init__(self, size, num_bombs):
+    def __init__(self, size, num_bombs, coords_bombs):
         self.size = size
         self.board_state = [[0 for i in range(size)] for j in range(size)]
         self.count_bombs = num_bombs
-        self.bomb_coords = []
+        self.bomb_coords = coords_bombs
         self.init_board()
-        self.input_bombs()
+        self.mark_bombs()
         self.init_board_val()
 
     def init_board(self):
@@ -42,14 +42,14 @@ class Minesweeper():
                                 numMines = numMines + 1
                     self.board_state[col][row] = numMines
 
-    def input_bombs(self):
-        for i in range(self.count_bombs):
-            x,y = input().strip().split(',')
-            x = int(x)
-            y = int(y)
-            self.bomb_coords.append([x,y])
+    def mark_bombs(self):
+        # for i in range(self.count_bombs):
+        #     x,y = input().strip().split(',')
+        #     x = int(x)
+        #     y = int(y)
+        #     self.bomb_coords.append([x,y])
         
-        for i,j in self.bomb_coords:
+        for (i,j) in self.bomb_coords:
             self.board_state[i][j] = -1
 
     def print_board(self):
@@ -59,18 +59,42 @@ class Minesweeper():
             print()
 
 if __name__ == "__main__":
-    size = int(input('Input size board : '))
-    num_bombs = int(input('Input number of bombs : '))
-    ms = Minesweeper(size, num_bombs)
+    try:
+        _filename = sys.argv[1]
+    except:
+        _filename = input('Input your game config file: ')
+        _file = open(_filename, 'r')
+    arr_bombs = []
+    i = 0
+    for f in _file:
+        if i == 0:
+            boardsize = int(f)
+        elif i == 1:
+            n_bombs = int(f)
+        else:
+            arr_bombs.append(tuple(f.split(',')))
+        i += 1
+
+    for i in range (len(arr_bombs)):
+        arr_bombs[i] = (int(arr_bombs[i][0]), int(arr_bombs[i][1]))
+
+    ms = Minesweeper(boardsize, n_bombs, arr_bombs)
     ms.print_board()
 
     clips = ClipsMinesweeper()
     clips.environment.reset()
-    clips.load_board(size, num_bombs)
+    clips.load_board(boardsize, n_bombs)
     clips.load_square(ms.board_state)
+
+    app = QApplication([])
+    window = Board(boardsize, n_bombs, ms.bomb_coords, clips)
+    app.exec_()
+    
+    # size = int(input('Input size board : '))
+    # num_bombs = int(input('Input number of bombs : '))
+    
     # clips.print_facts()
 
-    
     # clips.environment.run()
     # clips.print_facts()
     # while True :
@@ -83,7 +107,3 @@ if __name__ == "__main__":
     #         if fact.template == template_square: 
     #             print(fact['x'], ' ', fact['y'])
     #         i += 1
-
-    app = QApplication([])
-    window = Board(size, num_bombs, ms.bomb_coords, clips)
-    app.exec_()
